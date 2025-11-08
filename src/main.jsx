72dc854f02e1a68357fd8a708e3687993238411b
Prakash UI <prakash.kattoju@gmail.com>
2025-11-08 13:25:27 +0530

    6th round changes

--- src/main.jsx
+++ src/main.jsx
@@ -14,7 +14,7 @@ import './index.css'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/">
         <App />
       </BrowserRouter>
     </Provider>
