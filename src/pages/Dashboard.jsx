98d6d79d4c68b2d67be74707ab22854e4c087638
Prakash UI <prakash.kattoju@gmail.com>
2025-11-14 16:47:08 +0530

    16th sub css changes

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -83,8 +83,8 @@ export default function Dashboard() {
           />
         </div>
         <div className="form-group d-flex gap-2">
-          <input className="form-control" type="number" value={activeToken} onChange={(e) => setActiveToken(e.target.value)} placeholder="Search by Token..." autoComplete="off" />
-          <input className="form-control" type="number" value={activeMobile} onChange={(e) => setActiveMobile(e.target.value)} placeholder="Search by mobile..." autoComplete="off" maxLength={10} />
+          <input className="form-control" type="number" value={activeToken} onChange={(e) => setActiveToken(e.target.value)} placeholder="Token..." autoComplete="off" />
+          <input className="form-control" type="number" value={activeMobile} onChange={(e) => setActiveMobile(e.target.value)} placeholder="Mobile..." autoComplete="off" maxLength={10} />
           {!searchActive ? <button disabled={activeMobile === "" && activeToken === ""} onClick={() => handleSearch()} type='button' className='form-control btn'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z" /></svg></button> : <button onClick={() => handleSearchCancel()} type='button' className='form-control btn'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg></button>}
         </div>
       </div>
