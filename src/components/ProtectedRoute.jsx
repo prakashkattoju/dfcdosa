c0c9c192bfafeff733288d5ed32c750ae577c80b
Prakash UI <prakash.kattoju@gmail.com>
2025-11-17 12:52:14 +0530

    sorry, i think it is last commit

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -1,13 +1,10 @@
 import { useState, useCallback, useEffect, useRef } from 'react';
-import { useFormik } from "formik";
-import * as Yup from "yup";
 import { Navigate, useNavigate, useLocation, Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { setUserDetails } from '../store/userSlice';
 import { decodeToken } from 'react-jwt';
 import { logOut } from '../store/authSlice';
-import { GetUserByID, UpdateUserName } from '../services/Userservices';
-import { FaSpinner } from "react-icons/fa";
+import { GetUserByID } from '../services/Userservices';
 
 const ProtectedRoute = ({ children, allowedRoles = [] }) => {
   const dispatch = useDispatch();
@@ -21,7 +18,6 @@ const ProtectedRoute = ({ children, allowedRoles = [] }) => {
   const user_role = decodedToken?.user_role;
   const [showMenu, setShowMenu] = useState(false)
 
-  const [showPromptModal, setShowPromptModal] = useState(false)
   const [loading, setLoading] = useState(false);
 
   const fetchuser = useCallback(async () => {
@@ -69,39 +65,6 @@ const ProtectedRoute = ({ children, allowedRoles = [] }) => {
 
   const user = useSelector((state) => state.user);
 
-  useEffect(() => {
-    user.fullname === "" && setShowPromptModal(true)
-  }, [user]);
-
-  // Formik initialization
-  const formik = useFormik({
-    initialValues: {
-      uname: ""
-    },
-    validationSchema: Yup.object({
-      uname: Yup.string()
-        .required("Name is required")
-    }),
-    onSubmit: async (values) => {
-      try {
-        setLoading(true)
-        const data = await UpdateUserName(values.uname, user_id);
-        if (data.status) {
-          document.activeElement?.blur();
-          setShowPromptModal(false)
-          dispatch(setUserDetails({
-            ...user,
-            fullname: values.uname,
-          }))
-        }
-      } catch (error) {
-        console.error("Error", error)
-      } finally {
-        setLoading(false)
-      }
-    },
-  });
-
   if (!isLoggedIn) {
     return <Navigate to="/" />;
   }
@@ -129,7 +92,7 @@ const ProtectedRoute = ({ children, allowedRoles = [] }) => {
           </div>
         </div>
         {user_role === "user" && <div className="login-user">
-          <span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-480q-60 0-102-42t-42-102q0-60 42-102t102-42q60 0 102 42t42 102q0 60-42 102t-102 42ZM192-192v-96q0-23 12.5-43.5T239-366q55-32 116.29-49 61.29-17 124.5-17t124.71 17Q666-398 721-366q22 13 34.5 34t12.5 44v96H192Zm72-72h432v-24q0-5.18-3.03-9.41-3.02-4.24-7.97-6.59-46-28-98-42t-107-14q-55 0-107 14t-98 42q-5 4-8 7.72-3 3.73-3 8.28v24Zm216.21-288Q510-552 531-573.21t21-51Q552-654 530.79-675t-51-21Q450-696 429-674.79t-21 51Q408-594 429.21-573t51 21Zm-.21-72Zm0 360Z" /></svg> {user.fullname}</span>
+          <span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M480-480q-60 0-102-42t-42-102q0-60 42-102t102-42q60 0 102 42t42 102q0 60-42 102t-102 42ZM192-192v-96q0-23 12.5-43.5T239-366q55-32 116.29-49 61.29-17 124.5-17t124.71 17Q666-398 721-366q22 13 34.5 34t12.5 44v96H192Zm72-72h432v-24q0-5.18-3.03-9.41-3.02-4.24-7.97-6.59-46-28-98-42t-107-14q-55 0-107 14t-98 42q-5 4-8 7.72-3 3.73-3 8.28v24Zm216.21-288Q510-552 531-573.21t21-51Q552-654 530.79-675t-51-21Q450-696 429-674.79t-21 51Q408-594 429.21-573t51 21Zm-.21-72Zm0 360Z" /></svg> {user.fullname ? user.fullname : 'User'}</span>
           <span><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M280-40q-33 0-56.5-23.5T200-120v-720q0-33 23.5-56.5T280-920h400q33 0 56.5 23.5T760-840v124q18 7 29 22t11 34v80q0 19-11 34t-29 22v404q0 33-23.5 56.5T680-40H280Zm0-80h400v-720H280v720Zm0 0v-720 720Zm200-600q17 0 28.5-11.5T520-760q0-17-11.5-28.5T480-800q-17 0-28.5 11.5T440-760q0 17 11.5 28.5T480-720Z" /></svg> {user.mobile}</span>
         </div>}
         {(user_role === "admin" && location.pathname !== '/order-details') && <ul id="mobileMenu" className={`navbar-nav ms-auto ${showMenu && 'show'}`}>
@@ -155,37 +118,6 @@ const ProtectedRoute = ({ children, allowedRoles = [] }) => {
           </div>
         </article>
       </main>
-      <div
-        className={`dfc-modal modal fade ${showPromptModal ? "show d-flex" : ""}`}
-        id="PromptModal"
-        tabIndex="-1"
-      >
-        <div className="modal-dialog">
-          <form className="modal-content" onSubmit={formik.handleSubmit}>
-            <div className="modal-header">
-              <h4 className="modal-title">Your Name</h4>
-            </div>
-            <div className="modal-body">
-              <div className="form-group">
-                <input
-                  name="uname"
-                  placeholder="Enter your name to submit"
-                  value={formik.values.uname}
-                  onChange={formik.handleChange}
-                  onBlur={formik.handleBlur}
-                  className="form-control"
-                />
-                {formik.touched.uname && formik.errors.uname ? (
-                  <div className="input-error">{formik.errors.uname}</div>
-                ) : null}
-              </div>
-            </div>
-            <div className="modal-footer">
-              <button type="submit" className="btn"> {loading && <FaSpinner className="animate-spin" />} Submit </button>
-            </div>
-          </form>
-        </div>
-      </div>
     </div>
   );
 };
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,36 +1,58 @@
 import { useEffect, useState } from 'react'
+import { useFormik } from "formik";
+import * as Yup from "yup";
 import priceDisplay from '../util/priceDisplay';
 import { useDispatch, useSelector } from 'react-redux';
 import { incrementQuantity, decrementQuantity } from '../store/cartSlice';
-import { verifyCartItems } from '../store/cartThunks';
+import { setUserDetails } from '../store/userSlice';
+//import { verifyCartItems } from '../store/cartThunks';
 import { useNavigate } from "react-router-dom";
-import ConfirmModal from '../components/ConfirmModal';
+import { decodeToken } from 'react-jwt';
 import { CreateBill } from '../services/Billservices';
+import { UpdateUserName } from '../services/Userservices';
+
+import { FaSpinner } from "react-icons/fa";
 
 export default function Cart() {
-
+    const user = useSelector((state) => state.user);
     const cart = useSelector((state) => state.cart.cart);
+    const token = useSelector((state) => state.auth.token);
     const dispatch = useDispatch();
     const navigate = useNavigate();
     //const [removedItems, setRemovedItems] = useState([])
+    const decodedToken = decodeToken(token);
+    const user_id = decodedToken?.user_id;
     const [loading, setLoading] = useState(false);
-    const [showConfirm, setShowConfirm] = useState({
-        title: null,
-        show: false
-    });
+    const [showPromptModal, setShowPromptModal] = useState(false)
 
-    /* useEffect(() => {
-        const handleVerify = async () => {
-            setLoading(true)
-            const { availableItems, removedItems } = await dispatch(verifyCartItems()).unwrap();
-            if (removedItems.length > 0) {
-                setRemovedItems(removedItems)
-
+    // Formik initialization
+    const formik = useFormik({
+        initialValues: {
+            uname: ""
+        },
+        validationSchema: Yup.object({
+            uname: Yup.string()
+                .required("Name is required")
+        }),
+        onSubmit: async (values) => {
+            try {
+                setLoading(true)
+                const data = await UpdateUserName(values.uname, user_id);
+                if (data.status) {
+                    handleCancel();
+                    dispatch(setUserDetails({
+                        ...user,
+                        fullname: values.uname,
+                    }))
+                    handleConfirmSubmit()
+                }
+            } catch (error) {
+                console.error("Error", error)
+            } finally {
+                setLoading(false)
             }
-            setLoading(false)
-        }
-        handleVerify()
-    }, []); */
+        },
+    });
 
     const getQuantity = (product_id) => {
         const qty = cart.find((el) => el.product_id === product_id);
@@ -57,10 +79,7 @@ export default function Cart() {
 
     const handleCancel = () => {
         document.activeElement?.blur();
-        setShowConfirm({
-            title: null,
-            show: false
-        });
+        setShowPromptModal(false);
     };
 
     const handleConfirmSubmit = async () => {
@@ -130,10 +149,7 @@ export default function Cart() {
                     <div className="cart-summary-badge">
                         <div className="cart-bottom-bar"><strong className="total-count">{getCartQuantity()}</strong> | <strong className="total-cart">{getCartAmount()}</strong></div>
                         <div className="continue">
-                            <button className="btn" onClick={() => setShowConfirm({
-                                title: null,
-                                show: true
-                            })}>Submit</button>
+                            <button className="btn" onClick={() => setShowPromptModal(true)}>Submit</button>
                         </div>
                     </div>
                 </> :
@@ -143,14 +159,56 @@ export default function Cart() {
                     </div>
                 </div>
             }
-            <ConfirmModal
-                show={showConfirm.show}
-                title="Confirm"
-                message={`Are you sure you want to place your order?`}
-                onConfirm={handleConfirmSubmit}
-                onConfirmLabel="Yes, Place Order"
-                onCancel={handleCancel}
-            />
+            <div
+                className={`dfc-modal modal fade ${showPromptModal ? "show d-flex" : ""}`}
+                id="PromptModal"
+                tabIndex="-1"
+            >
+                <div className="modal-dialog">
+                    {user.fullname === "" ? <form className="modal-content" onSubmit={formik.handleSubmit}>
+                        <div className="modal-header">
+                            <h4 className="modal-title">Enter Name & Confirm Order</h4>
+                        </div>
+                        <div className="modal-body">
+                            <div className="form-group">
+                                <input
+                                    name="uname"
+                                    placeholder="Your name"
+                                    value={formik.values.uname}
+                                    onChange={formik.handleChange}
+                                    onBlur={formik.handleBlur}
+                                    className="form-control"
+                                />
+                                {formik.touched.uname && formik.errors.uname ? (
+                                    <div className="input-error">{formik.errors.uname}</div>
+                                ) : null}
+                            </div>
+                        </div>
+                        <div className="modal-footer">
+                            <button className="btn btn-secondary" onClick={handleCancel}>
+                                Cancel
+                            </button>
+                            <button type="submit" className="btn"> {loading && <FaSpinner className="animate-spin" />} Submit </button>
+                        </div>
+                    </form> :
+                        <div className="modal-content">
+                            <div className="modal-header">
+                                <h4 className="modal-title">Confirm Order</h4>
+                            </div>
+                            <div className="modal-body">
+                                <p>{`Are you sure you want to place your order?`}</p>
+                            </div>
+                            <div className="modal-footer">
+                                <button className="btn btn-secondary" onClick={handleCancel}>
+                                    No
+                                </button>
+                                <button className="btn" onClick={handleConfirmSubmit}>
+                                    {loading && <FaSpinner className="animate-spin" />} Yes
+                                </button>
+                            </div>
+                        </div>}
+                </div>
+            </div>
         </>
     )
 }
