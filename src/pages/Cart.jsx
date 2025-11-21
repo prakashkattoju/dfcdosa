7760c90a4143ddef0faaf67c76b30eae75a19705
Prakash UI <prakash.kattoju@gmail.com>
2025-11-21 22:29:29 +0530

    cart changes

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -102,8 +102,6 @@ export default function Cart() {
             }
         } catch (error) {
             console.error("Error", error.message)
-        } finally {
-            setLoading(false)
         }
     }
 
@@ -143,8 +141,7 @@ export default function Cart() {
                 <div onClick={() => setShowConfirm(true)} className='p-1'><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" /></svg></div>
             </div>
         </header>
-
-        {loading ? <div className="list"><p className='text-center'>Loading...</p></div> : <div>
+        <div>
             {/* {removedItems.length > 0 && <div className="cart-summary-review removed">
                 <h3>Sorry, the below item(s) are not available</h3>
                 <div className="tbl-cart show-cart">
@@ -258,7 +255,7 @@ export default function Cart() {
                 onConfirmLabel="Yes"
                 onCancel={handleExitCancel}
             />
-        </div>}
+        </div>
     </>
     )
 }
