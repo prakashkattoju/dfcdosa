98d268fae1ccb09f05f6742f3127b5ebd39a8f3c
Prakash UI <prakash.kattoju@gmail.com>
2026-01-02 19:23:55 +0530

    02-01-26 prakash

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -194,9 +194,9 @@ export default function Home() {
         <>
             <header ref={headerRef} className="site-header">
                 <div className='search-area d-flex gap-2 align-items-center justify-content-between'>
-                    <button className='icon-btn' onClick={backBtn}>
+                    {/* <button className='icon-btn' onClick={backBtn}>
                         <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="currentColor"><path d="M240-200h120v-240h240v240h120v-360L480-740 240-560v360Zm-80 80v-480l320-240 320 240v480H520v-240h-80v240H160Zm320-350Z" /></svg>
-                    </button>
+                    </button> */}
                     <div className="search-form">
                         <div className="form-group">
                             <input ref={searchInputRef} className="form-control" type="text" value={queryString} onChange={(e) => setSearchResultsFunc(e.target.value)} placeholder="Search here..." autoComplete="off" disabled={loading} />
